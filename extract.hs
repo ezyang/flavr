a0f86fc079792cf4482b65b2a394aca8e5bc0e40
Edward Z. Yang <ezyang@mit.edu>
2012-12-19 14:00:58 +0800

    Checkpoint
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- extract.hs
+++ extract.hs
@@ -38,43 +38,40 @@ handleFile = map handleIngredient
 -- XXX deal with parentheticals
 -- XXX deal with "esp."
 
+-- this is a pretty cool trick: turn runs of tags into tokens and then
+-- parse over that
 parsePred p = token renderTags (const (initialPos "<unknown>")) (\x -> if p x then Just x else Nothing)
 tag t = parsePred (any (~== t))
 ptag cl = tag (TagOpen "p" [("class", cl)])
 
-parseIngredient :: Parsec [[Tag String]] u ()
 parseIngredient = do
-    ingredient <- (ptag "lh" <|> ptag "lh1")
+    ingredient <- fmap (strip . map toLower . innerText) (ptag "lh" <|> ptag "lh1")
     -- sometimes the preamble is marked in the classes; take advantage
     -- appropriately
     many (try (many (ptag "ul") >> many1 (ptag "ul3")))
     optional (try (many1 (ptag "ul") >> lookAhead (ptag "ul1")))
     -- parse the actual things (and combine them together)
-    matches <- many (ptag "ul1" <|> ptag "ul") -- some ingredients don't have pairings, so don't use many1
+    -- it's an OR because sometimes there are interspersed other
+    -- sections, and after they conclude you get back an ul1
+    matches' <- many (ptag "ul1" <|> ptag "ul") -- some ingredients don't have pairings, so don't use many1
     -- get rid of the extra bits
     optional (try (many (ptag "h4" >> many (ptag "ul"))))
     -- done
     eof
-    -- trace (show (dropWhile (not . filterEntry . tail) matches)) $ return ()
-
-handleIngredient x =
-    let name = strip . map toLower . fromTagText . head . tail $ x
-        entries = map handleEntry
+    -- post-processing
+    let matches = map handleEntry
                 . filter filterEntry
                 . map (takeWhile (~/= TagClose "p")) -- doesn't work if there is nested p, fortunately, there is not!
-                . takeWhile (all (~/= TagOpen "p" [("class", "h4")])) -- chop off pairings
-                . takeWhile (all (~/= TagText "AVOID")) -- chop off AVOID (not a map, because AVOID is a divider)
-                -- . dropWhile (any (~== TagOpen "p" [("class", "ul3")]))
-                -- . map tail
-                -- [<p class="ul">beans</p>]
-                . partitions (\y -> y ~== TagOpen "p" [("class", "ul")] || y ~== TagOpen "p" [("class", "ul1")])
-                -- <p class="lh1">INGREDIENT</p><p class="ul">beans</p>
+                . takeWhile (all (~/= TagText "AVOID"))
+                $ matches'
+    return (Entry ingredient matches)
+
+handleIngredient x =
+    let Right r = runParser parseIngredient () "<unknown>"            -- regex over token structure
+                . filter ((`elem` datum) . fromAttrib "class" . head) -- drop irrelevant tokens
+                . partitions (~== TagOpen "p" [])                     -- tokenize
                 $ x
-        Right foo = rt $ runParser parseIngredient () "<unknown>" (filter ((`elem` datum) . fromAttrib "class" . head) . partitions (~== TagOpen "p" []) $ x)
-        structure = filter (`elem` datum) . map (fromAttrib "class" . head) . partitions (~== TagOpen "p" []) $ x
-    in -- trace name $ trace (show structure) $
-       -- pp (map innerText . filter ((`elem` meta) . fromAttrib "class" . head)  . partitions (~== TagOpen "p" []) $ x)
-       foo `seq` Entry name entries
+    in r
 
 rt a = trace (show a) $ a
 pp xs a = (unsafePerformIO $ mapM_ print xs) `seq` a
@@ -84,7 +81,7 @@ meta = ["p1", "ca", "img", "boxh", "ext", "exts", "ca3", "", "sbh", "sbtx1", "sb
 
 -- XXX deal with tips that bleed over into the list (maybe do an alpha check)
 filterEntry x =
-    not (head x ~== TagOpen "strong" [] && innerText [head (tail x)] `elem` ["Season:", "Taste:", "Weight:", "Volume:", "Botanical relatives:", "Function:", "Techniques:", "Techniques/Tips:", "Botanical relative:"])
+    not (head (tail x) ~== TagOpen "strong" [] && innerText [head (tail (tail x))] `elem` ["Season:", "Taste:", "Weight:", "Volume:", "Botanical relatives:", "Function:", "Techniques:", "Techniques/Tips:", "Botanical relative:"])
 
 handleEntry x =
     let name = strip . innerText $ x
@@ -101,5 +98,5 @@ collapse (x:xs) = x : go x xs
           go _ [] = []
 
 as = sort ((map (:[]) (['a'..'i'] ++ ['m','s','t'])) ++ ["jkl", "nop", "qr"])
-v a = readFile ("TFB/OEBPS/Text/FlavorBible_chap-3" ++ a ++ ".html")
-r = concatMap (handleFile . parseTags) `fmap` mapM v as
+v a = unsafePerformIO $ readFile ("TFB/OEBPS/Text/FlavorBible_chap-3" ++ a ++ ".html")
+r = concatMap (handleFile . parseTags) $ map v as
